fe49e15168c683afde990d080ee5b64ab11f1a94
Robert Senderek <robert.senderek@10g.pl>
2025-07-18 07:41:32 +0200

    [WIP] qualcommax: ipq50xx: Add support for Zyxel SCR50AXE
    // ath11k doesn't support two QCN6122 and by will only run 2.4+5Ghz for now #19670
    
    This is tri-band WiFi6E capable router. Also Zyxel Nebula managed so no real local GUI. To open device 4 screws must be located uder label .Four latches are on front and two on each side.
    FCC shows It's identical to WSQ65 sold as Zyxel Multy M6E but that's nowhare to be found yet. WSQ65 is not covered by this PR
    
    Speficiations:
    * SoC: Qualcomm IPQ5018 (800Mhz)
    * RAM: 1GB DDR3
    * UART: PCB "J3"  is located left from front LED strip
                  (VCC/TX/RX/     /GND)   3.3V 115200n8
    * Wi-Fi1: IPQ5018 (2x2 2.4 Ghz 802.11b/g/n/ax)
    * Wi-Fi2: QCN6102 (2x2:2 5 Ghz 802.11an/ac/ax)
    * Wi-Fi3: QCN6122 (2x2:2 6 Ghz 802.11an/ac/ax)
    * Ethernet: QCA8337 4xLAN 1Gbit / 1xWAN 1Gbit
    * Buttons: WPS , Reset
    * Flash: Winbond W25N02KWZEIR 256MB
    * LEDs: 13 in total
       RGB power, RGB info, RGB cloud, RGB wifi, Green WPS
    * FCCID: I8803891
    
    Flash Instructions:
    connect uart to J3 connector next to the front LEDs
    go to failsafe when this shows up in log:
    "Press the [f] key and hit [enter] to enter failsafe mode"
    execute:
    mount_root
    passwd -d root
    reboot
    
    now as soon as you see:
    'Please press Enter to activate this console.'
    login is root password is empty
    execute:
    fw_setenv DebugFlag=0x1
    fw_setenv bootdelay=0x2
    #since root password is overwritten to unknown each boot let's clear it out.
    passwd -d root
    backup ubi partition "rootfs" into safe space
    reboot
    
    *OpenWrt installation
    stop uboot and execute:
    setenv ipaddr 192.168.1.1
    setenv serverip 192.168.1.10
    tftpboot factory.ubi
    flash rootfs
    reset into openwrt
    
    *restore OEM from backup
    stop uboot and execute:
    setenv ipaddr 192.168.1.1
    setenv serverip 192.168.1.10
    tftpboot *-initramfs-uImage.itb
    bootm
    #now restore your backup to rootfs partition
    ubiformat /dev/mtd16 -y -f /tmp/rootfs.bin
    
    Signed-off-by: Robert Senderek <robert.senderek@10g.pl>

--- target/linux/qualcommax/ipq50xx/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq50xx/base-files/lib/upgrade/platform.sh
@@ -145,7 +145,8 @@ platform_do_upgrade() {
 		CI_ROOT_UBIPART="rootfs"
 		nand_do_upgrade "$1"
 		;;
-	yuncore,ax830)
+	yuncore,ax830|\
+	zyxel,scr50axe)
 		CI_UBIPART="rootfs"
 		remove_oem_ubi_volume ubi_rootfs
 		remove_oem_ubi_volume bt_fw
